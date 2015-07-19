48b468ee941e82fdb0989e939f697720b7df44d0
Bradley Iroine <bradplos@iroine.com>
2015-07-19 09:41:59 -0600

    Main files

---
+++ main.js
@@ -0,0 +1,18 @@
+/*
+* www.github.com/bradplos/new-tab-page
+*/
+
+window.onload = getLink;
+
+/* ADDS HREF ELEMENT TO AN ID
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– */
+function getLink()
+{
+
+  /* SECTION
+  –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– */
+  $('#add id here').attr('href','add url here'); /*copy / repeat this line for links.*/
+
+
+
+}
