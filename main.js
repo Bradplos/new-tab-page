8a730fa88f2b988b9faae39039b2c851618aed58
Bradley Iroine <bradplos@iroine.com>
2015-07-26 19:17:25 -0600

    changed template to full

--- main.js
+++ main.js
@@ -9,10 +9,86 @@ window.onload = getLink;
 function getLink()
 {
 
-  /* SECTION
+  /* CODE [CODE]
   –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– */
-  $('#add id here').attr('href','add url here'); /*copy / repeat this line for links.*/
+  $('#jsfiddle').attr('href','https://jsfiddle.net');
+  $('#pastebin').attr('href','https://pastebin.com/');
+  $('#github').attr('href','https://github.com');
+  $('#stacko').attr('href','https://stackoverflow.com');
+  $('#materialcss').attr('href','http://materializecss.com/');
+  $('#gridfr').attr('href','http://gridframework.com/');
+  $('#code-guide').attr('href','http://mdo.github.io/code-guide/');
+  $('#dev-html').attr('href','https://developer.mozilla.org/en-US/docs/Web/HTML');
+  $('#dev-css').attr('href','https://developer.mozilla.org/en-US/docs/Web/CSS');
+  $('#dev-js').attr('href','https://developer.mozilla.org/en-US/docs/Web/JavaScript');
+  $('#dev-cpp').attr('href','http://cplusplus.com/doc/tutorial/');
 
+/* REDDIT [RDDT]
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– */
+  $('#rddt').attr('href','https://reddit.com');
+  $('#rddt-pcmr').attr('href','https://reddit.com/r/pcmasterrace');
+  $('#rddt-adva').attr('href','https://reddit.com/r/adviceanimals');
+  $('#rddt-gamedev').attr('href','https://reddit.com/r/gamedev');
+  $('#rddt-webdev').attr('href','https://reddit.com/r/webdev');
+  $('#rddt-prog').attr('href','https://reddit.com/r/programming');
+  $('#rddt-pixel').attr('href','https://reddit.com/r/pixelart');
+  $('#rddt-crap').attr('href','https://reddit.com/r/crappydesign');
+  $('#rddt-gw2').attr('href','https://reddit.com/r/guildwars2');
+  $('#rddt-eve').attr('href','https://reddit.com/r/eve');
 
+  /* #_CHANS [CHAN]
+  –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– */
+  $('#4c-g').attr('href','https://boards.4chan.org/g/');
+  $('#4c-wg').attr('href','https://boards.4chan.org/wg/');
+  $('#8c-tech').attr('href','https://8ch.net/tech/');
+  $('#8c-agdg').attr('href','https://8ch.net/agdg/');
+  $('#8c-prog').attr('href','https://8ch.net/prog/');
+  $('#8c-rice').attr('href','https://8ch.net/rice/');
+  $('#8c-wg').attr('href','https://8ch.net/wg/');
+
+  /* NEWS [NEWS]
+  –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– */
+  $('#bbc').attr('href','https://bbc.com/');
+  $('#ars').attr('href','https://arstechnica.com/');
+  $('#tnw').attr('href','https://thenextweb.com/');
+  $('#wired').attr('href','http://wired.com/');
+  $('#medium').attr('href','https://medium.com/top-stories');
+  $('#hacker').attr('href','https://news.ycombinator.com/');
+
+  /* SOCIAL [SOCL]
+  –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– */
+  $('#twitter').attr('href','https://twitter.com/');
+  $('#tumblr').attr('href','https://tumblr.com');
+  $('#ello').attr('href','https://ello.co');
+
+  /* MEDIA [MDIA]
+  –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– */
+  $('#imgur').attr('href','https://imgur.com/hot/time');
+  $('#youtube').attr('href','https://youtube.com/feed/subscriptions');
+  $('#twitch').attr('href','http://twitch.tv');
+  $('#netflix').attr('href','https://netflix.com');
+
+  /* GAMES [GMES]
+  –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– */
+  $('#kong').attr('href','https://kongregate.com/');
+  $('#gw2-meta').attr('href','http://metabattle.com/wiki/MetaBattle_Wiki');
+  $('#gw2-timer').attr('href','http://howar31.com/GW2Timer/');
+
+  /* LEARNING [LERN]
+  –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– */
+  $('#wolfram').attr('href','http://wolframalpha.com/');
+  $('#desmos').attr('href','http://desmos.com/calculator');
+  $('#khan').attr('href','https://khanacademy.org/');
+  $('#duolingo').attr('href','https://duolingo.com/');
+  $('#bibme').attr('href','http://bibme.org/');
+  $('#dictionary').attr('href','http://dictionary.reference.com/');
+  $('#thesaurus').attr('href','http://thesaurus.com/');
+
+  /* UTILITIES [UTIL]
+  –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– */
+  $('#amazon').attr('href','https://amazon.com');
+  $('#humble').attr('href','https://humblebundle.com/');
+  $('#gmail').attr('href','https://mail.google.com/mail/u/0/?pli=1#inbox');
+  $('#bitly').attr('href','https://bitly.com/');
 
 }
